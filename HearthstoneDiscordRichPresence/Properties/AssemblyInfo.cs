ea35bae748895c48acefe61053938117488f5503
supersam710 <spillers.sam@gmail.com>
2021-06-09 16:47:27 -0400

    Major Overhaul

--- HearthstoneDiscordRichPresence/Properties/AssemblyInfo.cs
+++ HearthstoneDiscordRichPresence/Properties/AssemblyInfo.cs
@@ -5,21 +5,28 @@ using System.Runtime.InteropServices;
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
+// General assembly information is controlled by the following
+// attribute set. Change the values of these attributes to modify the information
+// associated with the assembly.
 [assembly: AssemblyTitle("HearthstoneDiscordRichPresence")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HearthstoneDiscordRichPresence")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
 // dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
 // COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+// Setting ComVisible to false makes types in this assembly invisible
+// for COM components. If you need to access a type in this assembly with
+// COM, set to true for a ComVisible attribute of this type.
 [assembly: ComVisible(false)]
 
 // Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+// The following GUID is the type library identifier when this project is exposed to COM
 [assembly: Guid("711b0a31-6b97-4c9d-971e-55a7f30cfc07")]
 
 // Informacje o wersji zestawu zawierają następujące cztery wartości:
@@ -32,5 +39,15 @@ using System.Runtime.InteropServices;
 // Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
+// The assembly version information contains the following four values:
+//
+// Major version
+// Minor version
+// Build number
+//      Correction
+//
+// You can specify all values or use the default build and revision numbers
+// using the symbol "*" as shown below:
+// [assembly: AssemblyVersion ("1.0. *")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
